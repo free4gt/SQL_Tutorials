f9b904f8de3952eaac1fdc52f47a8ad947709002
Chris <ckottmyer3@gatech.edu>
2026-02-13 17:57:36 -0500

    removeing refs

--- admin/containers/postgres/database_setup/databases/car_factory/tables/model.sql
+++ admin/containers/postgres/database_setup/databases/car_factory/tables/model.sql
@@ -1,6 +1,6 @@
 CREATE TABLE model (
     model_id SERIAL PRIMARY KEY,
-    make_id INT REFERENCES make(make_id),
+    make_id INT,
     model_name VARCHAR(50) NOT NULL,
     base_cost DECIMAL(10,2),
     intro_date DATE
